5e0a40289c8b4b001ace163d8a9c139bd70b6f47
goott3 <AlEridaniB@gmail.com>
2023-10-16 10:27:56 +0900

    커밋테스트

---
+++ SprotGearMall/src/main/java/edu/spring/mall/domain/ProductVO.java
@@ -0,0 +1,85 @@
+package edu.spring.mall.domain;
+
+public class ProductVO {
+	private int productId;
+	private String productName;
+	private int productprice;
+	private int productStock;
+	private String productMaker;
+	private String productImgPath;
+	private String productCategory;
+	private int productIsDeleted;
+	public ProductVO(int productId, String productName, int productprice, int productStock, String productMaker,
+			String productImgPath, String productCategory, int productIsDeleted) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.productprice = productprice;
+		this.productStock = productStock;
+		this.productMaker = productMaker;
+		this.productImgPath = productImgPath;
+		this.productCategory = productCategory;
+		this.productIsDeleted = productIsDeleted;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getProductprice() {
+		return productprice;
+	}
+	public void setProductprice(int productprice) {
+		this.productprice = productprice;
+	}
+	public int getProductStock() {
+		return productStock;
+	}
+	public void setProductStock(int productStock) {
+		this.productStock = productStock;
+	}
+	public String getProductMaker() {
+		return productMaker;
+	}
+	public void setProductMaker(String productMaker) {
+		this.productMaker = productMaker;
+	}
+	public String getProductImgPath() {
+		return productImgPath;
+	}
+	public void setProductImgPath(String productImgPath) {
+		this.productImgPath = productImgPath;
+	}
+	public String getProductCategory() {
+		return productCategory;
+	}
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+	public int getProductIsDeleted() {
+		return productIsDeleted;
+	}
+	public void setProductIsDeleted(int productIsDeleted) {
+		this.productIsDeleted = productIsDeleted;
+	}
+//staging test
+	public ProductVO() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "ProductVO [productId=" + productId + ", productName=" + productName + ", productprice=" + productprice
+				+ ", productStock=" + productStock + ", productMaker=" + productMaker + ", productImgPath="
+				+ productImgPath + ", productCategory=" + productCategory + ", productIsDeleted=" + productIsDeleted
+				+ "]";
+	}
+	
+	
+}
