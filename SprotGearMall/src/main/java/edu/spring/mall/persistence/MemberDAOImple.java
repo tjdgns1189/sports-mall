478eaea426d77b2caf6687edaa0c32992dbbfdc1
goott3 <AlEridaniB@gmail.com>
2023-10-16 11:54:55 +0900

    member insert 성공

--- SprotGearMall/src/main/java/edu/spring/mall/persistence/MemberDAOImple.java
+++ SprotGearMall/src/main/java/edu/spring/mall/persistence/MemberDAOImple.java
@@ -4,9 +4,12 @@ import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import edu.spring.mall.domain.MemberVO;
 
+
+@Repository
 public class MemberDAOImple implements MemberDAO {
 	private static final Logger logger = LoggerFactory.getLogger(MemberDAOImple.class);
 	private static final String NAMESPACE = "edu.spring.mall.MemberMapper";
@@ -16,6 +19,7 @@ public class MemberDAOImple implements MemberDAO {
 	
 	@Override
 	public int insert(MemberVO vo) {
+		logger.info("dao insert »£√‚");
 		return sqlSession.insert(NAMESPACE + ".insert", vo);
 	}
 
--- SprotGearMall/src/test/java/edu/spring/mall/testDB.java
+++ SprotGearMall/src/test/java/edu/spring/mall/testDB.java
@@ -1,29 +1,45 @@
 package edu.spring.mall;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import edu.spring.mall.domain.MemberVO;
 import edu.spring.mall.persistence.MemberDAO;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
+
+@WebAppConfiguration
 public class testDB {
 	private static final Logger logger = LoggerFactory.getLogger(testDB.class);
-	
+
 	@Autowired
-	private static MemberDAO dao;
-	
+	private MemberDAO dao;
+
 	@Test
 	public void daoTest() {
-		
+
 		insert();
 	}
 
 	private void insert() {
+		MemberVO vo = new MemberVO("test", "1234", "홍길동", "010-1234-5678", "test@test.com", "서울특별시 성북구 성북로21길", "관리자");
 		logger.info("insert호출");
-		MemberVO vo = new MemberVO("test", "1234","홍길동", "010-1234-5678", "test@test.com","서울특별시 성북구 성북로21길", "관리자");
-		dao.insert(vo);
+		try {
+			dao.insert(vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+
+		logger.info("insert호출 후");
 	}
 
 }
