9744b37115ef3c3f14f509254049480ef7328b3e
goott3 <AlEridaniB@gmail.com>
2023-10-18 17:45:11 +0900

    로그인창 바뀜

--- SprotGearMall/src/main/java/edu/spring/mall/service/MemberServiceImple.java
+++ SprotGearMall/src/main/java/edu/spring/mall/service/MemberServiceImple.java
@@ -1,18 +1,31 @@
 package edu.spring.mall.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import edu.spring.mall.domain.MemberVO;
+import edu.spring.mall.persistence.MemberDAO;
 
 public class MemberServiceImple implements MemberService {
+	private final Logger logger = LoggerFactory.getLogger(MemberServiceImple.class);
+	
+	
+	@Autowired
+	private MemberDAO dao;
 
 	@Override
 	public int create(MemberVO vo) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		logger.info("create 호출 vo = " + vo.toString());
+		int result = dao.insert(vo);
+		return result;
 	}
 
 	@Override
 	public MemberVO read(String memberId, String password) throws Exception {
-		// TODO Auto-generated method stub
+		logger.info("read 호출");
+		logger.info("memberId : " + memberId);
+		MemberVO vo = dao.se
 		return null;
 	}
 
