5800dcfcb094427cc835a0f7eefff466739ab22f
goott3 <AlEridaniB@gmail.com>
2023-10-25 17:00:54 +0900

    develop ->master

---
+++ SprotGearMall/src/main/java/edu/spring/mall/service/QnaBoardService.java
@@ -0,0 +1,13 @@
+package edu.spring.mall.service;
+
+import java.util.List;
+
+import edu.spring.mall.domain.QnaBoardVO;
+import edu.spring.mall.pageutil.PageCriteria;
+
+public interface QnaBoardService {
+	int create(QnaBoardVO vo);
+	List<QnaBoardVO> read(PageCriteria criteria);	
+	QnaBoardVO read(int qnaBoardId);
+	int getTotalCounts();
+}
