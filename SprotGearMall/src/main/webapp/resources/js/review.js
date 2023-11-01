017739c54f0dfbcffe1ef59d38d20d55d3ab0988
goott3 <AlEridaniB@gmail.com>
2023-11-01 18:03:39 +0900

    11-01 집에 가기전

--- SprotGearMall/src/main/webapp/resources/js/review.js
+++ SprotGearMall/src/main/webapp/resources/js/review.js
@@ -1,30 +1,39 @@
-$(document).ready(function(){
-  // 별 클릭 이벤트
-  $('#star-rating .fa').on('click', function(){
-    var rating = $(this).data('value');
-    $('#star-rating .fa').each(function(){
-      if($(this).data('value') <= rating){
-        $(this).removeClass('fa-star-o').addClass('fa-star');
-      } else {
-        $(this).removeClass('fa-star').addClass('fa-star-o');
-      }
-    });
+$(()=>{
+  // 모든 리뷰하기 버튼에 대한 클릭 이벤트를 설정합니다.
+  $('.review-btn').click(function(){
+    // data-productid 속성을 사용하여 현재 클릭된 상품의 ID를 가져옵니다.
+    var productId = $(this).data('productid');
+
+    // 상품 ID를 기반으로 상품 정보를 불러오는 코드를 여기에 추가하십시오.
+    // 예시: $('#productName').text('상품명: ' + productName);
+
+    // 모달 창을 띄웁니다.
+    $('#reviewModal').modal('show');
   });
 
-  // 리뷰 모달을 보여주는 함수
-  window.reviewModal = function(productId) {
-    // 상품 정보 설정
-    $('#productName').text($('#' + productId).text());
-    // 별점 초기화
-    $('#star-rating .fa').removeClass('fa-star').addClass('fa-star-o');
-    // 모달 표시
-    $('#reviewModal').modal('show');
-  };
+  // 별 클릭 이벤트를 설정합니다.
+  $('#star-rating .fa').click(function(){
+    // 이 별의 인덱스를 가져옵니다.
+    var index = $(this).index();
+    
+    // 별점을 설정합니다.
+    $('#star-rating .fa').removeClass('fa-star').addClass('fa-star-o'); // 모든 별을 비활성화합니다.
+    $('#star-rating .fa:lt(' + (index+1) + ')').removeClass('fa-star-o').addClass('fa-star'); // 선택된 별과 그 이전의 모든 별을 활성화합니다.
+    
+    // 선택된 별점의 값을 어딘가에 저장하거나 이용할 수 있습니다.
+    // 예시: var ratingValue = index + 1;
+  });
 
-  // 리뷰 전송 버튼 클릭 이벤트
-  $('#submitReview').on('click', function(){
+  // 리뷰 전송 버튼 클릭 이벤트를 설정합니다.
+  $('#submitReview').click(function(){
+    // 리뷰 텍스트와 별점 값을 가져옵니다.
     var reviewText = $('#reviewText').val();
     var rating = $('#star-rating .fa-star').length;
-    // 리뷰와 별점 정보를 서버로 전송하는 로직을 여기에 작성
+
+    // 리뷰와 별점 정보를 서버로 전송하는 코드를 여기에 추가하십시오.
+    // 예시:
+    // $.post('/submit-review', { productId: productId, reviewText: reviewText, rating: rating }, function(response){
+    //   // 성공적으로 리뷰를 전송한 후의 동작을 여기에 추가하십시오.
+    // });
   });
 });
